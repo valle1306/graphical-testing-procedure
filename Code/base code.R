d61945ad777246050e4e2c7e11c2ecb4a8417a2d
Mengyang Yi <ymm723@icloud.com>
2025-07-14 22:07:25 +0800

    For simplification, I combine the ui + server together

--- Code/base code.R
+++ Code/base code.R
@@ -1,3 +1,45 @@
+library(shiny)
+library(visNetwork)
+library(DT)
+library(jsonlite)
+library(TrialSimulator)
+
+ui <- fluidPage(
+  titlePanel("Interactive Graphical Testing Editor (Start from Scratch)"),
+  
+  # Centered buttons
+  fluidRow(
+    column(12, align = "center",
+           actionButton("add_node", "Add Node", class = "btn btn-primary"),
+           actionButton("add_edge", "Add Edge", class = "btn btn-success"),
+           actionButton("delete_node", "Delete Node", class = "btn btn-danger"),
+           actionButton("delete_edge", "Delete Edge", class = "btn btn-danger")
+    )
+  ),
+  br(),
+  
+  fluidRow(
+    column(3,
+           wellPanel(
+             tags$div(style = "min-height: 650px;",
+                      fileInput("upload", "Upload JSON Graph File"),
+                      downloadButton("export", "Export"),
+                      actionButton("run_gt", "Reject the selected Hypothesis", class = "btn btn-warning"),
+                      tags$hr(),
+                      h5(tags$b("Node Table"), style = "color:blue"),
+                      dataTableOutput("node_table"),
+                      tags$hr(),
+                      h5(tags$b("Edge Table"), style = "color:darkgreen"),
+                      dataTableOutput("edge_table")
+             )
+           )
+    ),
+    column(9,
+           visNetworkOutput("graph", height = "600px")
+    )
+  )
+)
+
 server <- function(input, output, session) {
   rv <- reactiveValues(
     nodes = data.frame(label = character(0), alpha = numeric(0), stringsAsFactors = FALSE),
@@ -8,7 +50,7 @@ server <- function(input, output, session) {
   )
   
   graph_update_trigger <- reactiveVal(1)
-
+  
   
   get_transition_matrix <- function(nodes, edges) {
     n <- nrow(nodes)
@@ -31,7 +73,7 @@ server <- function(input, output, session) {
     asf <- rep("asOF", length(hs))  # Dummy ASF initialization
     max_info <- rep(100, length(hs))  # Default: max_info = 100 for all nodes
     transition <- get_transition_matrix(rv$nodes, rv$edges)
-   
+    
     
     
     # Run Graphical Testing and extract elements
@@ -61,7 +103,7 @@ server <- function(input, output, session) {
     })
   })
   
- 
+  
   # add node 
   observeEvent(input$add_node, {
     showModal(modalDialog(
@@ -364,7 +406,7 @@ server <- function(input, output, session) {
   
 }
 
-#shinyApp(ui, server)
+shinyApp(ui, server)
 
 
 
